37f3316868c8cd557fea6ef983f80e16faa4505f
SMITHSTEPH <stephanie-k-smith@uiowa.edu>
2015-09-29 22:07:19 -0500

    Test MVC commit

---
+++ FundamentalsOfSE/FundamentalsOfSE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FundamentalsOfSE.MvcApplication" Language="C#" %>
---
+++ FundamentalsOfSE/FundamentalsOfSE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FundamentalsOfSE.Startup))]
+namespace FundamentalsOfSE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
