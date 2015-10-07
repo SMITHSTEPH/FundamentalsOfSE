eca86c120940853c7e6106589e8a37b0f4ed8586
SMITHSTEPH <stephanie-k-smith@uiowa.edu>
2015-10-07 00:19:02 -0500

    created Member/Person/Admin model and sign up pages for each. Looked into how to pass data between controller and view.

--- WebApplication2/WebApplication2/Controllers/AccountController.cs
+++ WebApplication2/WebApplication2/Controllers/AccountController.cs
@@ -9,16 +9,44 @@ namespace WebApplication2.Controllers
 {
     public class AccountController : Controller
     {
+        private Models.Account account = new Models.Account();
+        private Models.Member member = new Models.Member();
+        private Models.Leader leader = new Models.Leader();
+        private Models.Administrator admin = new Models.Administrator();
         // GET: Account
         public ActionResult Index()
         {
             Debug.Print("In Index controller");
             return View();
         }
-        public ActionResult SignUp()
+        //[HttpPost]
+        public ActionResult SignUpMember(FormCollection Form)
         {
+            
+           // Debug.Print(Form.ToString());
             Debug.Print("In Sign Up controller");
             return View();
         }
+        public ActionResult SignUpAdmin()
+        {
+            return View();
+        }
+        public ActionResult SignUpLeader()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(String Username, String Password)
+        {
+            if(account.isUserValid(Username, Password))
+            {
+                return View("SuccessTest");
+            }
+            else
+            {
+                return Redirect("Index");
+            }
+        }
+        
     }
 }
