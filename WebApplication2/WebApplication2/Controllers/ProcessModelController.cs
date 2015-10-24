aa93afc5abed46013ebe07bbc5b4c6c8d54a25e3
SMITHSTEPH <stephanie-k-smith@uiowa.edu>
2015-10-24 13:19:45 -0500

    Stephanie is hopefully just adding a new MVC and not app_data or bin or obj/Debug

---
+++ WebApplication2/WebApplication2/Controllers/ProcessModelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ProcessModelController : Controller
+    {
+        // GET: ProcessModel
+        public ActionResult RequirementQuestions()
+        {
+            return View();
+        }
+        public ActionResult ProcessModelResult()
+        {
+            return View();
+        }
+    }
+}
