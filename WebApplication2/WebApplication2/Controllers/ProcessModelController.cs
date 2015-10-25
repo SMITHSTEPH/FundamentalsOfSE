8486021d972e45d3d5c703f49f08af3e83e3b40a
SMITHSTEPH <stephanie-k-smith@uiowa.edu>
2015-10-24 22:36:30 -0500

    Started adding more to ProcessModel MC

--- WebApplication2/WebApplication2/Controllers/ProcessModelController.cs
+++ WebApplication2/WebApplication2/Controllers/ProcessModelController.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class ProcessModelController : Controller
     {
+        ProcessModel PModel = new ProcessModel();
         // GET: ProcessModel
         public ActionResult RequirementQuestions()
         {
             return View();
         }
-        public ActionResult ProcessModelResult()
+        [HttpPost]
+        public ActionResult ProcessModelResult(FormCollection form)
         {
+            int[] Answers = { };
+            form.CopyTo(Answers, 0);
+            Debug.Print("Length of form is: ");
+            Debug.Print(Answers.Length.ToString());
+
+            PModel.ProcessAnswers(Answers);
             return View();
         }
     }
--- WebApplication2/WebApplication2/Models/ProcessModel.cs
+++ WebApplication2/WebApplication2/Models/ProcessModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,56 @@ namespace WebApplication2.Models
 {
     public class ProcessModel
     {
+        enum ProcessModels{Waterfall, IterativeWaterfall, RAD, Agile, COTS};
+        ArrayList ProcessModelsList = new ArrayList();
+        private ProcessModels DB = new ProcessModels(); //instance of process model Database
+        /**
+        These 6 Dictionaries give us insight on what a 'winning' process model looks like
+        **/
+        Dictionary<int, Array> WaterfallData = new Dictionary<int, Array>();
+        Dictionary<int, Array> IterativeWaterfallData = new Dictionary<int, Array>();
+        Dictionary<int, Array> RADData = new Dictionary<int, Array>();
+        Dictionary<int, Array> AgileData = new Dictionary<int, Array>();
+        Dictionary<int, Array> COTSDAta= new Dictionary<int, Array>();
+        //mystery
+        /**
+        Constructor populates an ArrayList with all of the ProcessModel tables in the database
+        **/
+        public ProcessModel()
+        {
+           foreach (ProcessModels processModel in Enum.GetValues(typeof(ProcessModel)))
+           {
+                ProcessModelsList.Add(processModel);
+           }
+        }
+        /**
+        Adds the submitted form to the answers databases
+        **/
+        public void ProcessAnswers(Array answers)
+        {
+            for(int i=0; i<answers.Length; i++)
+            {
+                //DB.Account.Add(answers);
+            }
+        }
+        public void TrainData()
+        {
+          
+        }
+       public ArrayList EliminateProcessModels()
+        {
+            //for a given question
+            //for a given process model
+            //if answer in DB !=equal answer for priority 5 questions
+            //remove process model from arraylist of models
+            //
+            
+            return null;
+        }
+        public void ChooseProcessModels()
+        {
+
+        }
+
     }
 }
