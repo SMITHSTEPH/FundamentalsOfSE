232bdf933de56c45113815082f76ca4cc2ac63c1
brad-bergeron <brad-bergeron@uiowa.edu>
2015-10-28 16:15:18 -0500

    Minor Updates

--- WebApplication2/WebApplication2/Controllers/ProjectController.cs
+++ WebApplication2/WebApplication2/Controllers/ProjectController.cs
@@ -16,11 +16,11 @@ namespace WebApplication2.Controllers
             return View("ExistingProjects", ListofProjects);
         }
 
-        public ActionResult AddProjects()
+        public ActionResult AddProjects(Project ListofProjects)
         {
 
             //Needs to go to the page that allows them to add a project
-            return View("AddProjects");
+            return View("AddProjects", ListofProjects);
         }
 
         public ActionResult SumbitProjet()
